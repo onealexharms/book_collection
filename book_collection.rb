d57a501de1a96f7e6478eff3a61e96a66ab5e98e
Alex Harms <onealexharms@gmail.com>
2024-08-23 19:39:21 -0400

    Source file is a string

--- book_collection.rb
+++ book_collection.rb
@@ -1,7 +1,9 @@
 class BookCollection
   attr_reader :sourceFileName
+  attr_reader :sourceFile
 
   def initialize(filename)
     @sourceFileName = filename
+    @sourceFile = ""
   end
 end
--- test/test_book_collection.rb
+++ test/test_book_collection.rb
@@ -4,12 +4,15 @@ require 'minitest/autorun'
 require 'minitest/pride'
 
 class TestBookCollection < Minitest::Test
-  def test_source_file_name_is_index_md
-    bookCollection = BookCollection.new("filename")
+
+  def test_source_file_name_is_passed
+    bookCollection = BookCollection.new("filename")  
     assert_equal("filename", bookCollection.sourceFileName)
   end
 
-  def sourceFile_is_a_string
-    assert_kind_of(String, sourceFile)
+  def test_sourceFile_is_a_string
+    bookCollection = BookCollection.new("filename")  
+    assert_kind_of(String, bookCollection.sourceFile)
   end
+  
 end

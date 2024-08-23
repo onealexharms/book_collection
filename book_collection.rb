038cd2553d7acbb83961707bdc90e4bd95ee9ae2
Alex Harms <onealexharms@gmail.com>
2024-08-23 18:52:26 -0400

    sourceFile -> sourceFileName

--- book_collection.rb
+++ book_collection.rb
@@ -1,7 +1,7 @@
 class BookCollection
-  attr_reader :sourceFile
+  attr_reader :sourceFileName
 
   def initialize
-    @sourceFile = "index.md"
+    @sourceFileName = "index.md"
   end
 end
--- test/test_book_collection.rb
+++ test/test_book_collection.rb
@@ -6,20 +6,20 @@ require 'minitest/pride'
 class TestBookCollection < Minitest::Test
   def test_source_file_name_is_index_md
     bookCollection = BookCollection.new
-    assert_equal("index.md", bookCollection.sourceFile)
+    assert_equal("index.md", bookCollection.sourceFileName)
   end
 
   def test_source_file_exists
     bookCollection = BookCollection.new
-    assert File.exist?(bookCollection.sourceFile)
+    assert File.exist?(bookCollection.sourceFileName)
   end
 
   def test_source_file_has_expected_stuff
     bookCollection = BookCollection.new
-    contents = IO.read(bookCollection.sourceFile)    
+    contents = IO.read(bookCollection.sourceFileName)    
     assert contents.include?(".jpg")
     assert contents.include?(".webp")
     assert contents.include?("# ")
   end    
-
+  
 end

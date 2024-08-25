318c9f36c69d6b1d7c041cf7edabdeac5a707c48
Alex Harms <onealexharms@gmail.com>
2024-08-25 17:12:37 -0400

    Uninteresting refactor

--- test/test_book_collection.rb
+++ test/test_book_collection.rb
@@ -4,25 +4,27 @@ require 'minitest/autorun'
 require 'minitest/pride'
 
 class TestBookCollection < Minitest::Spec
+  @@source_file_path = "test/test_data/fake_index.md"
 
   describe "book collection" do
-    @source_file_path = "test/test_data/fake_index.md"
      
     it "has a source file" do
       source_file_path = "test/test_data/fake_index.md"
-      _(File.exist?(source_file_path)).must_equal(true)
+      _(File.exist?(@@source_file_path)).must_equal(true)
     end
 
     it "identifies an author at the top" do
       source_file = ["#blee blue", "froggy foo"]
       book_collection = BookCollection.new(source_file)
-      _(book_collection.authors.first).must_equal("blee blue")
+      _(book_collection.authors.first)
+        .must_equal("blee blue")
     end
 
     it "identifies two authors, ignoring a non-author line" do
       source_file = ["#birdy boo", "##froggy foo", "#goopy goo"] 
       book_collection = BookCollection.new(source_file)
-      _(book_collection.authors).must_equal(["birdy boo", "goopy goo"])
+      _(book_collection.authors)
+        .must_equal(["birdy boo", "goopy goo"])
     end
   end
 end

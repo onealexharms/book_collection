4f72aa1b9852b6866c67f5bdf87a4a47dfea81b1
Alex Harms <onealexharms@gmail.com>
2024-08-25 15:18:18 -0400

    regex is extracted

--- book_collection.rb
+++ book_collection.rb
@@ -1,3 +1,4 @@
+AUTHOR_FINDER = Regexp.new /^[\#](?<header>[^#].*)/ 
 class BookCollection
   attr_reader :source_file
 
@@ -8,7 +9,7 @@ class BookCollection
   def authors
     authors = []
     @source_file.each do |line| 
-      found_stuff = line.match(/^[\#](?<header>[^#].*)/)
+      found_stuff = line.match(AUTHOR_FINDER)
       if found_stuff && found_stuff[:header]
          authors << found_stuff[:header]
       end
--- test/test_book_collection.rb
+++ test/test_book_collection.rb
@@ -20,17 +20,3 @@ class TestBookCollection < Minitest::Spec
     end
   end
 end
-
-=begin
-[{:author_name => "Ada Palmer", 
-  :books => 
-    [{:title => "Seven Surrenders", :image => seven_surrenders.jpg, :commentary => commentary.md} 
-    {:title => zzz, :image => kljadfg, commentary => sokmehting.md}, 
-    {:title => "Too Like the Lightning", :image => image95.jpg, :commentary => commentary.md],
-{:author_name => "Ursula LeGuin", 
-  :books => 
-  [{:title => "The Left Hand of Darkness", :image => image42.png, :commentary => commentary.md}, 
-  {:title => "The Word for World is Forest", :image => image523.webp, :commentary => commentary.md},
-  {:title => "A Wizard of Earthsea", :image => y.img, :commentary = z.md}]]
-
-=end

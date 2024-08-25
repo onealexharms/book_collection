9cf07cbb4a70b60a5541a330c4d092f02293b0dc
Alex Harms <onealexharms@gmail.com>
2024-08-25 15:58:50 -0400

    .map is better

--- book_collection.rb
+++ book_collection.rb
@@ -1,19 +1,13 @@
-AUTHOR_FINDER = Regexp.new /^[\#](?<header>[^#].*)/ 
+HEADER_LINE = Regexp.new /^[\#][^#].*/
 class BookCollection
-  attr_reader :source_file
 
-  def initialize(source_file)
+  def initialize source_file
     @source_file = source_file
   end
 
   def authors
-    authors = []
-    @source_file.each do |line| 
-      found_stuff = line.match(AUTHOR_FINDER)
-      if found_stuff && found_stuff[:header]
-         authors << found_stuff[:header]
-      end
-    end
-    authors
+    @source_file
+      .select {|line| line.match(HEADER_LINE) }
+      .map {|header_line| header_line[1..-1]}
   end
 end

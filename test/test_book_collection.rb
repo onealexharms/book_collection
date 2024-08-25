bd94f9d15322fe500f18152561b57b3129978d5a
Alex Harms <onealexharms@gmail.com>
2024-08-25 17:02:13 -0400

    Found the source file

--- test/test_book_collection.rb
+++ test/test_book_collection.rb
@@ -6,6 +6,12 @@ require 'minitest/pride'
 class TestBookCollection < Minitest::Spec
 
   describe "book collection" do
+    @source_file_path = "test/test_data/fake_index.md"
+     
+    it "has a source file" do
+      source_file_path = "test/test_data/fake_index.md"
+      _(File.exist?(source_file_path)).must_equal(true)
+    end
 
     it "identifies an author at the top" do
       source_file = ["#blee blue", "froggy foo"]

46d0dbaaef685344bba4b89bd5b3a9c88b25dcbe
Alex Harms <onealexharms@gmail.com>
2024-08-29 17:17:54 -0400

    Refactor

--- book_collection.rb
+++ book_collection.rb
@@ -6,6 +6,60 @@ class BookCollection
     write_directories_for author_names_to_paths
   end
 
+  def is_author? line
+    author_line = Regexp.new /^[\#][^#].*/
+    line.match? author_line
+  end
+
+  def author_names
+    @source_file
+      .select {|line| is_author? line}
+      .map {|header_line| header_line[1..-1].strip}
+  end
+
+  def author_names_to_paths
+    author_names.map {|author| path_version_of author}
+  end
+
+  def path_version_of name, path=''
+    path + name.downcase.gsub(" ", "-")
+  end
+
+  def write_directories_for paths
+    paths&.each {|directory_name|
+      path = @target_file_path + directory_name
+      unless File.directory?(path)
+        FileUtils.mkpath(path)
+      end                
+    }
+  end
+
+  def image_filenames
+    @source_file
+      .select {|line| is_image? line}
+      .map {|image_line| 
+        image_line_to_filename image_line
+      }
+  end
+
+  def image_line_to_filename image_line
+    image_line
+      .gsub("|", "")
+      .gsub("(", "")
+      .gsub(")", "")
+      .gsub("![]", "")
+      .strip
+  end
+
+  def is_world_level? line
+    world_level_line = Regexp.new /^[#][#]\s/
+    line.match? world_level_line
+  end
+  
+  def is_image? line
+    image_line  = Regexp.new /!\[\]\(.*\)/
+    line.match? image_line 
+  end
   def title(line, i)
     title = ''
     if is_title_beginning? line
@@ -34,60 +88,8 @@ class BookCollection
     titles - ['']
   end
 
-  def author_names
-    @source_file
-      .select {|line| is_author? line}
-      .map {|header_line| header_line[1..-1].strip}
-  end
-
-  def author_names_to_paths
-    author_names.map {|author| path_version_of author}
-  end
-
-  def path_version_of name
-    name.downcase.gsub(" ", "-")
-  end
-
-  def write_directories_for paths
-    paths.each {|directory_name|
-      path = @target_file_path + directory_name
-      unless File.directory?(path)
-        FileUtils.mkpath(path)
-      end                
-    }
-  end
-
-  def author_names
-    @source_file
-      .select {|line| is_author? line}
-      .map {|header_line| header_line[1..-1].strip}
-  end
-
-  def image_filenames
-    @source_file
-      .select {|line| is_image? line}
-      .map {|image_line| 
-        image_line_to_filename image_line
-      }
-  end
-
-  def image_line_to_filename image_line
-    image_line
-      .gsub("|", "")
-      .gsub("(", "")
-      .gsub(")", "")
-      .gsub("![]", "")
-      .strip
-  end
-
-  def is_author? line
-    author_line = Regexp.new /^[\#][^#].*/
-    line.match? author_line
-  end
-
-  def is_image? line
-    image_line  = Regexp.new /!\[\]\(.*\)/
-    line.match image_line 
+  def titles_to_paths
+    titles.map {|title| path_version_of title}
   end
 
 end
